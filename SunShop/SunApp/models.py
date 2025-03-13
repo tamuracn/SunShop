4fcded5538922e05450dba3c5534a6960baf4cff
Clara Tamura <clarant2@uw.edu>
2025-03-12 17:20:50 -0700

    new code

--- SunShop/SunApp/models.py
+++ SunShop/SunApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+"""
+Everytime you make changes to the models.py file, you need to run the following commands into terminal:
+python manage.py makemigrations
+python manage.py migrate
+
+"""
+class TodoItem(models.Model):
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    # content = models.TextField()
+    # date = models.DateTimeField(auto_now_add=True)
+    # def __str__(self):
+    #     return self.content
+
--- SunShop/SunApp/views.py
+++ SunShop/SunApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'home.html')
+
+def todo(request):
+    items = TodoItem.objects.all()
+    return render(request, 'todo.html',{"todo":items})
